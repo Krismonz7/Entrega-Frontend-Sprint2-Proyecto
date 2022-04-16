190c3a6621aad45d3a184678fb999fd88112a99f
krismonz7 <kriz.monz707@gmail.com>
2022-04-16 12:55:12 -0500

    Agrego mas preguntas - QuestionarioFigma.js

--- src/formFigma/QuestionarioFigma.js
+++ src/formFigma/QuestionarioFigma.js
@@ -3,8 +3,36 @@ export const questionGeek = [
         question: "¿Qué es Figma?",
         a: "Lenguaje Interpretado",
         b: "Herramienta de generacion de prototipos",
-        c: "Gestin de bases de datos",
+        c: "Gestion de bases de datos",
         d: "Lenguaje Binario",
         corrrect: "Herramienta de generacion de prototipos"
+    },{
+        question: "¿Quien creo Figma?",
+        a: "Apple",
+        b: "Microsoft",
+        c: "Softsell",
+        d: "Ninguna de las anteriores",
+        corrrect: "Ninguna de las anteriores"
+    },{
+        question: "¿Para que sirve Figma?",
+        a: "Entorno de desarrollo web",
+        b: "Generar prototipos y modelos de alta fidelidad",
+        c: "Entorno de desarrllo Back-end",
+        d: "Ninguna de las anteriores",
+        corrrect: "Generar prototipos y modelos de alta fidelidad"
+    },{
+        question: "¿Que es un muckup?",
+        a: "Fabricacion de una maqueta",
+        b: "Framework",
+        c: "Extencion de Visual Studio",
+        d: "Plataforma",
+        corrrect: "Fabricacion de una maqueta"
+    },{
+        question: "¿Figma permite llevar u maquetado a un modelo de alta fidelidad?",
+        a: "No",
+        b: "Masomenos",
+        c: "Posiblemente",
+        d: "Si",
+        corrrect: "Si"
     }
 ]
