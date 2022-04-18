9ca0d447e3ea3a9706b52d0d0a6c26bb10addc5a
krismonz7 <kriz.monz707@gmail.com>
2022-04-18 10:51:28 -0500

    Agrego el resto de la rutas - Rutes.js

--- src/routes/Rutes.js
+++ src/routes/Rutes.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import QuestionHTML from '../formHTML/FormHTML';
+import QuestionHTML from '../formHTML/FormHTML'
 import QuestionCSS from '../formCSS/FormCSS'
+import QuestionUX from '../formUX/FormUX'
+import QuestionJS from '../formJS/FormJs'
+import QuestionFigma from '../formFigma/FormFigma'
 import Containerprincipal from '../container/conatiner'
 import {BrowserRouter as Router,
   Route,
@@ -13,8 +16,10 @@ export default function Rutas() {
       <Routes>
         <Route path='/FormularioHTML' element={<QuestionHTML/>} />
         <Route path='/FormularioCSS' element={<QuestionCSS/>} />
+        <Route path='/FormularioFigma' element={<QuestionFigma/>} />
+        <Route path='/FormularioJS' element={<QuestionJS/>} />
+        <Route path='/FormularioUX' element={<QuestionUX/>} />
         <Route path='/' element={<Containerprincipal/>} />
-
       </Routes>
     </Router>
   )
